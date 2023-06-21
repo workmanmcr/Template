2d49ca72f9ede0b8b9e07df8e5ee98507adbce98
workmanmcr <workmanmcr@gmail.com>
2023-06-20 19:48:30 -0700

    test prototype code

--- _tests_/triangle.test.js
+++ _tests_/triangle.test.js
@@ -8,9 +8,16 @@ describe('Triangle', () => {
     expect(triangle.side2).toEqual(4);
     expect(triangle.side3).toEqual(5);
   });
-});
-
 test('should correctly determine whether three lengths are not a triangle', () => {
   const notTriangle = new Triangle(3,9,22);
   expect(notTriangle.checkType()).toEqual("not a triangle");
+});
+test('should correctly determine whether three lengths make a scalene triangle', () => {
+  const scalTriangle = new Triangle(4,5,7)
+  expect(scalTriangle.checkType()).toEqual("scalene triangle");
+});
+test('should correctly determine whether three lengths make an isosceles triangle', () => {
+  const isoscTriangle = new Triangle(5,5,7)
+  expect(isoscTriangle.checkType()).toEqual("isosceles triangle");
+});
 });
